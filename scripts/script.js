5acaeedea310b2fcec8b4f5eb91817edb99e74e2
Emmanuel Malik <emmanuel00440@gmail.com>
2025-04-16 04:58:20 +0100

    mobile completed and desktop in progress

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const shareContainer = document.querySelector('.js-share-part');
+const michelleId = document.querySelector('.js-figure');
+const shareElement = document.querySelector('.js-share-button-div');
+const shareElementButton = document.querySelector('.share-button');
+const newIcons = document.querySelector('.js-share-icons');
+const shareTextElement = document.querySelector('.js-share-text');
+const iconElement = document.querySelectorAll('js-icon');
+const container = document.querySelector('.js-container');
+const triangleElement = document.querySelector('.js-triangle');
+
+
+
+shareElement.addEventListener('click', () => {
+  clickedShareButton()
+});
+
+shareElementButton.addEventListener('click', () => {
+  clickedShareButton()
+});
+
+function clickedShareButton() {
+  shareElement.classList.add('updated-share-button-div');
+  shareElementButton.classList.add('updated-share-button');
+  container.classList.add('update-container');
+  shareContainer.classList.add('new-share-container');
+  michelleId.classList.add('remove-figure');
+  newIcons.classList.add('display-new-icons');
+  shareTextElement.classList.add('display-share-text');
+  iconElement.classList.add('display-icon');
+  triangleElement.classList.add
+}
